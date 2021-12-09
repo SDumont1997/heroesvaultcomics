253e8d62195e19407ab5ac2e445e10e076df2ed1
SDumont1997 <sdumont1997@gmail.com>
2021-12-09 13:23:05 -0300

    added character postmapping and creationDto

--- src/main/java/com/mindhub/ecommerce/controllers/CharacterRestController.java
+++ src/main/java/com/mindhub/ecommerce/controllers/CharacterRestController.java
@@ -1,12 +1,20 @@
 package com.mindhub.ecommerce.controllers;
 
 
+import com.mindhub.ecommerce.dtos.CharacterCreationDTO;
 import com.mindhub.ecommerce.dtos.CharacterDTO;
+import com.mindhub.ecommerce.models.Author;
+import com.mindhub.ecommerce.models.Character;
+import com.mindhub.ecommerce.models.Publisher;
+import com.mindhub.ecommerce.services.IAppUserService;
+import com.mindhub.ecommerce.services.IAuthorService;
 import com.mindhub.ecommerce.services.ICharacterService;
+import com.mindhub.ecommerce.services.IPublisherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -18,10 +26,46 @@ import static java.util.stream.Collectors.toSet;
 public class CharacterRestController {
 
     @Autowired
+    private IAppUserService appUserService;
+
+    @Autowired
     private ICharacterService characterService;
+
+    @Autowired
+    private IAuthorService authorService;
+
+    @Autowired
+    private IPublisherService publisherService;
 
     @GetMapping("/characters")
     public Set<CharacterDTO> getCharacters(){
         return characterService.getAll().stream().map(CharacterDTO::new).collect(toSet());
+    }
+
+    @GetMapping("/characters/{id}")
+    public CharacterDTO getCharacterById(@PathVariable Long id){
+        return new CharacterDTO(characterService.getById(id));
+    }
+
+    @PostMapping("/characters")
+    public ResponseEntity<Object> createCharacter(Authentication authentication, @RequestBody CharacterCreationDTO characterCreationDTO){
+        String characterName = characterCreationDTO.getName();
+        String characterAlias = characterCreationDTO.getAlias();
+        String characterFirstAppearance = characterCreationDTO.getFirstAppearance();
+        String characterBirthPlace = characterCreationDTO.getBirthPlace();
+        Author characterCreator = authorService.getById(characterCreationDTO.getAuthorId());
+        Publisher characterPublisher = publisherService.getById(characterCreationDTO.getPublisherId());
+        if (!appUserService.getByEmail(authentication.getName()).isAdmin()){
+            return new ResponseEntity<>("User does not have authorization for this action", HttpStatus.FORBIDDEN);
+        }
+        if (characterName.isEmpty() || characterAlias.isEmpty() || characterFirstAppearance.isEmpty() || characterBirthPlace.isEmpty() || characterCreationDTO.getAuthorId() == null || characterCreationDTO.getPublisherId() == null){
+            return new ResponseEntity<>("Missing parameters", HttpStatus.BAD_REQUEST);
+        }
+        if (characterCreator == null || characterPublisher == null){
+            return new ResponseEntity<>("Invalid author or publisher id", HttpStatus.BAD_REQUEST);
+        }
+        Character newCharacter = new Character(characterName, characterAlias, characterFirstAppearance, characterBirthPlace, characterCreator, characterPublisher);
+        characterService.save(newCharacter);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/com/mindhub/ecommerce/dtos/CharacterCreationDTO.java
@@ -0,0 +1,84 @@
+package com.mindhub.ecommerce.dtos;
+
+public class CharacterCreationDTO {
+    private String name;
+    private String alias;
+    private String firstAppearance;
+    private String birthPlace;
+    private Long authorId;
+    private Long publisherId;
+
+    public CharacterCreationDTO() {
+
+    }
+
+    public CharacterCreationDTO(String name, String alias, String firstAppearance, String birthPlace, Long authorId, Long publisherId) {
+        this.name = name;
+        this.alias = alias;
+        this.firstAppearance = firstAppearance;
+        this.birthPlace = birthPlace;
+        this.authorId = authorId;
+        this.publisherId = publisherId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getFirstAppearance() {
+        return firstAppearance;
+    }
+
+    public void setFirstAppearance(String firstAppearance) {
+        this.firstAppearance = firstAppearance;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public Long getPublisherId() {
+        return publisherId;
+    }
+
+    public void setPublisherId(Long publisherId) {
+        this.publisherId = publisherId;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("CharacterCreationDTO{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", alias='").append(alias).append('\'');
+        sb.append(", firstAppearance='").append(firstAppearance).append('\'');
+        sb.append(", birthPlace='").append(birthPlace).append('\'');
+        sb.append(", authorId=").append(authorId);
+        sb.append(", publisherId=").append(publisherId);
+        sb.append('}');
+        return sb.toString();
+    }
+}
