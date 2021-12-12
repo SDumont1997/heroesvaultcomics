ccb5683cb7649aa3b835c8c3f0de727f27165ff5
unknown <farfan.carlos.mc@gmail.com>
2021-12-12 20:08:37 -0300

    Complete shopping cart

--- src/main/resources/static/web/scripts/store.js
+++ src/main/resources/static/web/scripts/store.js
@@ -32,24 +32,77 @@ const app = Vue.createApp({
         showComics(visible){
             this.comicsVisible = visible;
         },
-        addComicToCart(event){
-            let quantity = event.target.elements.quantity.value;
-            let comicId = event.target.elements.addComicItem.id;
 
-            this.totalItems += parseInt(quantity);
-            let comicInfo = this.comics.filter(c => comicId == c.id)[0];
-            this.cartItems.push({productId:comicId, productQuantity:parseInt(quantity), productType:'comic', productImg: comicInfo.coverImgUrl, productPrice: comicInfo.price})
-            this.totalAmount = this.cartItems.reduce(function(total, item){return total + item.productQuantity * item.productPrice;},0);
-        },
-        addMerchToCart(event){
-            let quantity = event.target.elements.quantity.value;
-            let merchId = event.target.elements.addMerchItem.id;
+        addProductToCart(event){
+            let quantity = parseInt(event.target.elements.quantity.value);
+            let productId = event.target.elements.addComicItem.id;
+            let productType = event.target.elements.productType.value;
 
-            this.totalItems += parseInt(quantity);
-            let merchInfo = this.merch.filter(c => merchId == c.id)[0];
-            this.cartItems.push({productId:merchId, productQuantity:parseInt(quantity), productType:'merch', productImg: merchInfo.imgUrl, productPrice: merchInfo.price})
-            this.totalAmount = this.cartItems.reduce(function(total, item){return total + item.productQuantity * item.productPrice;},0);
+            let products = this.cartItems.filter(p => productId == p.productId && productType == p.productType);            
+            
+            if(products.length == 0){
+                let productInfo = {};
+
+                if (productType == 'comic'){
+                    let comicInfo = this.comics.filter(c => productId == c.id)[0];
+                    productInfo = {productId:productId, productQuantity:quantity, productType:'comic', productImg: comicInfo.coverImgUrl, productPrice: comicInfo.price};
+                }  else {
+                    let merchInfo = this.merch.filter(c => productId == c.id)[0];
+                    productInfo = {productId:productId, productQuantity:quantity, productType:'merch', productImg: merchInfo.imgUrl, productPrice: merchInfo.price};
+                }                
+                
+                this.cartItems.push(productInfo);
+
+            }else{
+                products[0].productQuantity += quantity;
+            }
+
+            this.calculateTotals();
+            this.updateStock(productId, productType, quantity);
         },
+        calculateTotals(){
+            this.totalAmount = this.cartItems.reduce(function(total, item){return total + item.productQuantity * item.productPrice;},0);
+            this.totalItems = this.cartItems.reduce(function(total, item){return total + item.productQuantity}, 0);
+        },
+        removeItem(productId, productType){
+            let product = this.cartItems.filter(p => productId == p.productId && productType == p.productType)[0];
+            if(product.productQuantity > 1){
+                product.productQuantity -= 1;
+                this.updateStock(productId, productType, -1);
+            }
+        },
+        addItem(productId, productType){
+            let product = this.cartItems.filter(p => productId == p.productId && productType == p.productType)[0];
+            if(productType == 'comic'){
+                let comic = this.comics.filter(c => productId == c.id)[0];
+                if(comic.stock >= 1){
+                    product.productQuantity += 1;
+                    this.updateStock(productId, productType, 1);        
+                }
+            }else{
+                let merch = this.merch.filter(m => productId == m.id)[0];
+                if(merch.stock >= 1){
+                    product.productQuantity += 1;
+                    this.updateStock(productId, productType, 1);
+                }
+            }
+        },
+        updateStock(productId, productType, productQuantity){
+            if(productType == 'comic'){
+                let comic = this.comics.filter(c => productId == c.id)[0];
+                comic.stock -= productQuantity;
+            }else{
+                let merch = this.merch.filter(m => productId == m.id)[0];
+                merch.stock -= productQuantity;
+            }
+        },
+        removeCartItem(productId, productType){
+            let product = this.cartItems.filter(p => productId == p.productId && productType == p.productType)[0];
+            let index = this.cartItems.indexOf(product);
+            this.cartItems.splice(index, 1);
+            this.calculateTotals();
+            this.updateStock(productId, productType, -product.productQuantity);
+        }
     },
 
 })
