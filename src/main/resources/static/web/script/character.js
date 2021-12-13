d1c83141dd8f5a8e8a1ac163b545275fc6b269f1
MigueMaisares <miguemaisares@gmail.com>
2021-12-13 20:42:17 -0300

    mejora slider y apariciones

--- src/main/resources/static/web/script/character.js
+++ src/main/resources/static/web/script/character.js
@@ -14,6 +14,7 @@ const app = Vue.createApp({
             this.personaje = response.data
             console.log(response.data)
             this.apariciones = response.data.appearances
+            console.log(response.data.appearances)
         })
         .catch(error => console.log(error.message))
     },
